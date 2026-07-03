81678108aade6c10b078ec3848ebd0ed2220362f
Kaloyan Velev <kaloqnvelev1@gmail.com>
2026-07-03 12:04:05 +0300

    release

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ load_dotenv()
 
 db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@localhost:5432/postgres'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/postgres'
     app.config['SECRET_KEY'] = "52f0c854e544f440769cba3b9c4405bd8d791f0dd5c6d5a3673eba39146bec40"
 
 
